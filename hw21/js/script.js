3ddf6e242e17a65ed7bf6d98f34041bffad207e8
IvanKor <helloivankor@gmail.com>
2021-08-14 11:31:27 +0300

    hw part 2

--- hw21/js/script.js
+++ hw21/js/script.js
@@ -28,17 +28,22 @@ window.addEventListener('load', function() {
 function createSongsList(arr) {
   arr
   .forEach(element => {
-    let li = document.createElement('li');
-    li.innerHTML = element.name;
+    let li = document.createElement('li'),
+      textContainer = document.createElement('span');
 
-    addButton('Like', 'like', li, likeSong);
-    addButton('Delete', 'delete', li, deleteSong);
-    addHeart(li);
+    textContainer.innerHTML = element.name;
+    li.append(textContainer);
 
     if(element.isLiked) {
       li.classList.add('liked');
+      addButton('Unlike', 'active', li, likeSong);
+    } else {
+      addButton('Like', 'like', li, likeSong);
     }
 
+    addButton('Delete', 'delete', li, deleteSong);
+    addHeart(li);
+    
     songsList.append(li);
   });
   updateSongCounter();
@@ -54,6 +59,9 @@ function addButton(title, className, target, func) {
   let button = document.createElement('button');
   button.innerHTML = title;
   button.classList.add('button', className);
+  if(className == 'active') {
+    button.classList.add('like');
+  }
   button.addEventListener('click', func);
   target.append(button);
 }
@@ -64,12 +72,24 @@ function deleteSong(event) {
 }
 
 function likeSong(event) {
-  event.target.parentElement.classList.toggle('liked');
+  let clickedElement = event.target;
+  
+  clickedElement.parentElement.classList.toggle('liked');
+  clickedElement.classList.toggle('active');
+
+  if(clickedElement.classList.contains('active')) {
+    clickedElement.innerHTML = 'Unlike'
+  } else {
+    clickedElement.innerHTML = 'Like'
+  }
 }
 
 function addSong(songName) {
-  let li = document.createElement('li');
-  li.innerHTML = songName;
+  let li = document.createElement('li'),
+      textContainer = document.createElement('span');
+
+  textContainer.innerHTML = songName;
+  li.append(textContainer);
 
   addButton('Like', 'like', li, likeSong);
   addButton('Delete', 'delete', li, deleteSong);
